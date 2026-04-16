8f0bcfb352330e050dfa63813664f997d42703b8
tushardacoder <tusharbasak071@gmail.com>
2026-04-16 23:55:23 +0600

    saving all 16/04/2026

--- Codeforces_solution/Tower_of_boxes/obj/Debug/net10.0/Tower_of_boxes.AssemblyInfo.cs
+++ Codeforces_solution/Tower_of_boxes/obj/Debug/net10.0/Tower_of_boxes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tower_of_boxes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40ef21516c45718d8a1410dd1b9637a6a4035cce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tower_of_boxes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tower_of_boxes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
